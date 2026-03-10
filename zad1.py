4d018c4f2e73cda00d7a4f4b064b79c3637fd840
anjasustar <anja.sustar17@gmail.com>
2026-03-10 20:13:01 +0100

    Delete zad1.py

--- zad1.py
+++
@@ -1,8 +0,0 @@
-RadniSati=int(input("Unesi radne sate: "))
-EurPoSatu=float(input("Unesi satnicu: "))
-
-def total_euro():
-    return RadniSati*EurPoSatu
-
-Zarada=total_euro()
-print('Zaradila si: ', Zarada)
