de39198ac4aa31ebf65421f6845579daf33c2c97
anjasustar <anja.sustar17@gmail.com>
2026-03-10 20:11:15 +0100

    Add files via upload

---
+++ LV1/zad1.py
@@ -0,0 +1,8 @@
+RadniSati=int(input("Unesi radne sate: "))
+EurPoSatu=float(input("Unesi satnicu: "))
+
+def total_euro():
+    return RadniSati*EurPoSatu
+
+Zarada=total_euro()
+print('Zaradila si: ', Zarada)
